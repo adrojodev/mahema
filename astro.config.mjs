ae3a1cecc8d2e079efa911048c043f5dcc7e9cad
David Hernández <rojohernandez75@gmail.com>
2024-07-29 10:18:41 -0600

    Back on packages and changed vercel on config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-import vercelStatic from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "static",
-  adapter: vercelStatic(),
+  adapter: vercel(),
 });
